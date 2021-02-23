7cc1947c651205991d4cdd71a51b4fad9df3b351
Daniel Savu <savudani04@yahoo.ro>
2021-02-23 17:38:27 +0000

    feat: Remove redundant tpc definitions

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,14 @@
 declare const _default: {
     types: Array<{ types: Record<string, any> }>;
-    rpc: Record<string, any>;
-    providerRpc: Record<string, any>;
+    rpc: Record<string, Record<string, RpcFunctionDefinition>>;
     instances: Record<string, any>;
 };
 export default _default;
+export interface RpcFunctionDefinition {
+    description: string;
+    params: Array<{
+        name: string;
+        type: string;
+    }>;
+    type: string;
+}
